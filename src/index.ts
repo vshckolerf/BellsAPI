2dcd374d53995b9c846f1c9d88680f370c7ef5e4
MIMBOL228 <aekemerov@yandex.ru>
2023-08-03 09:10:35 +0500

    Звонки работают

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
 import 'dotenv/config'
-import { AppDataSource } from "./data-source"
+import {AppDataSource} from "./data-source"
 import {ConnectionManager} from "./classes/ConnectionManager";
-import { NtpTimeSync } from "ntp-time-sync";
+import {NtpTimeSync} from "ntp-time-sync";
 import {Lesson} from "./entity/Lesson";
 
 const ws_server_properties = {port: process.env.WS_SERVER_PORT};
 const timeSync = NtpTimeSync.getInstance();
 
 const getTime = async () => {
-    const now = new Date()
+    let now = new Date()
     try{
-        const {now} = await timeSync.getTime();
+        const ntpTime = await timeSync.getTime();
+        now = ntpTime.now
     } catch (e) {
-        // console.error("NTP", e);
+        console.error("NTP", e);
     }
     const hour = now.getHours();
     const minute = now.getMinutes();
+    const seconds = now.getSeconds();
 
-    console.log({hour, minute})
-    return {hour, minute};
+    console.log({hour, minute, seconds})
+    return {hour, minute, seconds};
 }
 
 AppDataSource.initialize().then(async () => {
-    const check = async () => {
-        const {hour, minute} = await getTime();
-
+    const getSoundsByTime = async ({hour, minute}) => {
         const start_lessons = await Lesson.getLessonsByStartTime(hour, minute);
         const end_lessons = await Lesson.getLessonsByEndTime(hour, minute);
 
@@ -33,18 +33,33 @@ AppDataSource.initialize().then(async () => {
         const start_sounds = start_lessons.map(l => l.class_range.start_sound);
         const end_sounds = end_lessons.map(l=> l.class_range.end_sound);
 
-        const sounds = start_sounds.concat(end_sounds);
-
+        return start_sounds.concat(end_sounds);
+    }
+    const check = async () => {
+        const time = await getTime();
+        const sounds = await getSoundsByTime(time);
         sounds.map(async (sound) => {
             const school_uuid = sound.school.uuid;
             console.log(school_uuid, "PLAY "+ sound.uuid)
             await connectionManager.sendToSchool(school_uuid, "PLAY "+ sound.uuid)
         });
+
+        const preSounds = await getSoundsByTime({hour: time.hour, minute: time.minute + 1});
+        preSounds.map(async (sound) => {
+            const school_uuid = sound.school.uuid;
+            console.log(school_uuid, "WARN "+ sound.uuid)
+            await connectionManager.sendToSchool(school_uuid, "WARN "+ sound.uuid)
+        });
     }
 
     const connectionManager = new ConnectionManager(ws_server_properties, ()=> {
-        console.log("Успешный запуск")
-        setInterval(check, 1000);
+        getTime().then(({seconds})=>{
+            setTimeout(()=>{
+                console.log("Успешный запуск")
+                check()
+                setInterval(check, 60000);
+            },seconds == 0 ? 1 : (60 - seconds) * 1000);
+        })
     });
 }).catch(console.error)
 
